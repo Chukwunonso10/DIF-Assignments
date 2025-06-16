8b82ca6cab0230c79b78ad4c4d469a5e26ed98a0
Chukwunonso10 <kuzuechinonsojude@gmail.com>
2025-06-16 19:50:08 +0100

    simplified atbash hasher

--- simple Hasher/hasher.js
+++ simple Hasher/hasher.js
@@ -1,9 +1,20 @@
-function atbashHasher(text) {
-  return text.toLowerCase().replace(/[a-z]/g, (match) => {
-    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
-    const index = alphabet.indexOf(match);
+function AtbashHasher(word) {
+  
+  //get the alphabet.
+ let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+ let pattern = /[a-z]/g;
+
+ //replacing each character in the word with its corresponding character in the reversed alphabet.
+ let result = word.toLowerCase().replace(pattern, (match) => {
+  //get the index of the letter in the alphabet.
+    let index = alphabet.indexOf(match);
     return alphabet[alphabet.length - 1 - index];
   });
+  return result;
 }
 
-console.log(atbashHasher('hello world'))
+console.log(AtbashHasher('g'))
+
+
+
+
